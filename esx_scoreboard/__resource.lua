ea5eab40d59827d96b7d51a44d0ad219611a9e75
Martin Riggs <63644849+wanderrer@users.noreply.github.com>
2020-06-01 09:18:14 +0300

    Add files via upload

---
+++ esx_scoreboard/__resource.lua
@@ -0,0 +1,28 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Scoreboard'
+
+version '1.0.0'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/martin.html'
+
+files {
+	'html/martin.html',
+	'html/css/site_global.css',
+	'html/css/index.css',
+	'html/listener.js',
+	'html/images/cart.png',
+	'html/images/doctor.png',
+	'html/images/film.png',
+	'html/images/money.png',
+	'html/images/police-u106-fr.png',
+	'html/images/repair.png',
+	'html/images/sarp_logo.png',
+	'html/images/taxi-front-yellow-icon.png',
+
+
+}
